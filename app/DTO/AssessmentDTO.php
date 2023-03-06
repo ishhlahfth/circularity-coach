b9332b13a002f20c4d69fd62a41552ed84dab260
Muhammad Ishlah Alfath <muhammadishlah@wehelpyou.id>
2023-03-06 19:02:56 +0700

    tambo cie

--- app/DTO/AssessmentDTO.php
+++ app/DTO/AssessmentDTO.php
@@ -6,29 +6,47 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class AssessmentDTO extends DataTransferObject
 {
-	private $name;
-	private $email;
-	private $whatsapp;
+    private $name;
+    private $email;
+    private $whatsapp;
+    private $secret;
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
-    public function setName($val) {
+    public function setName($val)
+    {
         $this->name = $val;
         return $this;
     }
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
-    public function setEmail($val) {
+    public function setEmail($val)
+    {
         $this->email = $val;
         return $this;
     }
-    public function getWhatsapp() {
+    public function getWhatsapp()
+    {
         return $this->whatsapp;
     }
-    public function setWhatsapp($val) {
+    public function setWhatsapp($val)
+    {
         $this->whatsapp = $val;
+        return $this;
+    }
+
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+
+    public function setSecret($val)
+    {
+        $this->secret = $val;
         return $this;
     }
 }
--- app/Http/Controllers/Web/AssessmentController.php
+++ app/Http/Controllers/Web/AssessmentController.php
@@ -14,16 +14,21 @@ class AssessmentController extends Controller
 
     public function __construct()
     {
-        $this->assessmentBusinessLayer = new AssessmentBusinessLayer();   
+        $this->assessmentBusinessLayer = new AssessmentBusinessLayer();
     }
 
-    public function generateAssessment(Request $request) {
+    public function generateAssessment(Request $request)
+    {
         $params = new AssessmentDTO();
         $params->setName($request->input('name'));
         $params->setEmail($request->input('email'));
         $params->setWhatsapp($request->input('phone'));
-        $result = $this->assessmentBusinessLayer->generateFreeAssessment($params);
-        return Redirect::to('https://www.circularity.coach/assessment');
+        $params->setSecret($request->input('secret'));
+        if ($params->getSecret() == 29) {
+            $result = $this->assessmentBusinessLayer->generateFreeAssessment($params);
+            return Redirect::to('https://www.circularity.coach/assessment');
+        } else {
+            return Redirect::to('https://www.circularity.coach/error');
+        }
     }
-
 }
